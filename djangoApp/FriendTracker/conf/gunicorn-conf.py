5dab28882dcab7b7aeda4c0bb16112780cb25432
AsafDov <NathenDov@gmail.com>
2024-11-27 12:24:20 +0200

    changed directory

--- djangoApp/FriendTracker/conf/gunicorn-conf.py
+++ djangoApp/FriendTracker/conf/gunicorn-conf.py
@@ -1,4 +1,4 @@
-command = '/home/asaf/Friend-Tracker/.venv/bin/gunicorn'
-pythonpath = '/home/asaf/Friend-Tracker/djangoApp'
+command = '/home/asaf/Desktop/Friend-Tracker/.venv/bin/gunicorn'
+pythonpath = '/home/asaf/Desktop/Friend-Tracker/djangoApp'
 bind = '10.100.102.9:8000'
 workers = 3
