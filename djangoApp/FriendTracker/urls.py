42c7c184a17e49cb3544bbdc805bd6363e87b9d4
AsafDov <NathenDov@gmail.com>
2025-01-21 17:04:39 +0200

    Trying to fix admin bug

--- djangoApp/FriendTracker/urls.py
+++ djangoApp/FriendTracker/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home', include("home.urls")),
+    path('home/', include("home.urls")),
     path('api/', include("api.urls")),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
 
 
 ]
